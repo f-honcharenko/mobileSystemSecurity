bda6b335d9861f88182efd2582e0fbc14e610744
Filipp <akstoplay@gmail.com>
2022-03-06 14:01:02 +0100

    update registerPage styles

--- class1/front/src/pages/auth/Register.vue
+++ class1/front/src/pages/auth/Register.vue
@@ -1,34 +1,61 @@
 <route lang="yaml">
 meta:
-  title: "[SN] Register"
+  title: "[SN] Login"
 </route>
 <script setup lang="ts">
 import { ref } from "vue";
+import { useRouter } from "vue-router";
+
+import useNotyf from "../../composable/useNotyf";
+
+const router = useRouter();
+const notyf = useNotyf();
 
 const handleRegister = async () => {
 	try {
-		alert("Succsess Register");
+		notyf.success("Successfully registered", 2000);
+		handleBack();
 	} catch (error) {
-		alert("Error");
+		notyf.error("Error while register", 2000);
 	}
+};
+const handleBack = () => {
+	router.push({ path: `/` });
 };
 </script>
 <template>
 	<div>
-		<title>[SN] Register</title>
-		<Header />
-		<div>
-			<label for="lgn-input-login">Login:</label>
-			<input type="text" id="lgn-input-login" />
-			<br />
-			<label for="lgn-input-password">Password:</label>
-			<input type="text" id="lgn-input-password" />
-			<br />
-			<label for="lgn-input-passwordConf">Password(confirm):</label>
-			<input type="text" id="lgn-input-passwordConf" />
-			<br />
-			<button @click="handleRegister">Register</button>
+		<div class="header">
+			<div class="back-btn btn" @click="handleBack">ᐸ</div>
 		</div>
-		<Footer />
+		<div>
+			<input type="text" class="login-input inp" placeholder="Login" /><br />
+			<input type="text" class="password-input inp" placeholder="Password" />
+			<input
+				type="text"
+				class="password-input inp"
+				placeholder="Confirm Password"
+			/>
+		</div>
+		<div>
+			<div class="btn login-btn" @click="handleRegister">Register</div>
+		</div>
 	</div>
 </template>
+<style scoped>
+.inp {
+	color: #fcfcfc;
+	border: none;
+	outline: none;
+	background-color: #252525;
+	font-size: xx-large;
+	margin: 40px;
+	/* font-size: ; */
+}
+.login-btn {
+	float: right;
+}
+.back-btn {
+	float: left;
+}
+</style>
